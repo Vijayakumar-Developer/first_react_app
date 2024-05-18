931024b5eb28d7dd241c0d662caa4fb2ccecd27e
Vijayakumar-Developer <vijai1993@gmail.com>
2024-05-18 17:32:09 +0530

    compoent call

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
 
 function App() {
 
-  function handleNameChange(){
-
-    const fruit=["apple","mango","orange"];
-    const int=Math.floor(Math.random()*3);
-    return fruit[int];
-  }
-  
   return (
-    <div className="App">
-          Learn React  
-          <p>{handleNameChange()}</p>
+    <div>
+          <Header />
     </div>
   );
 }
