054502cfd4417b08d7847d939c4bd73a5963fd11
Vijayakumar-Developer <vijai1993@gmail.com>
2024-05-25 17:50:38 +0530

    usestate textchange

--- src/Content.js
+++ src/Content.js
@@ -4,31 +4,26 @@ import { useState } from 'react';
 
 const Content = () => {
    
-   const [fruit ,setName]=useState("Apple");
-    function handleNameChange(){
-
-        const fruit=["apple","mango","orange"];
-        const int=Math.floor(Math.random()*3);
-        setName(fruit[int]);
-      }
-
-      // const [count, setCount]=useState(99);
-
-      // function incrementFunction(){
-      //   setCount(prevCount=>prevCount+1);
-      // }
-
-      // function decrementFunction(){
-      //   setCount(prevCount=>prevCount-1);
-      // }
-
+  const [items,setItems]=useState(
+    [
+      {id:1,checked:true,item:"Learn React"},
+      {id:2,checked:true,item:"Understand React"},
+      {id:3,checked:true,item:"Practice React"}
+    ]
+  );
   return (
     <main>
-     <p>I Like {fruit} </p>
-     
-     <button onClick={handleNameChange}>Subscripe  Button</button>
-     
-     
+      <ul>
+        
+          {items.map((item)=>(
+            <li>
+              <input type="checkbox" cheked={item.checked}/>
+              <label>{item.item}</label>
+              <button>Delete</button>
+              </li>
+          ))}
+        
+      </ul>
     </main>
     
   )
