68137d1194d2eb87dd6fa334c627d90c18364eed
Vijayakumar-Developer <vijai1993@gmail.com>
2024-05-25 22:12:13 +0530

    list

--- src/Content.js
+++ src/Content.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
 
 
 const Content = () => {
@@ -7,19 +8,37 @@ const Content = () => {
   const [items,setItems]=useState(
     [
       {id:1,checked:true,item:"Learn React"},
-      {id:2,checked:true,item:"Understand React"},
-      {id:3,checked:true,item:"Practice React"}
+      {id:2,checked:false,item:"Understand React"},
+      {id:3,checked:false,item:"Practice React"}
     ]
   );
+
+  const handleCheck=(id) =>{
+   const listItems =items.map((item)=>
+    item.id===id ?{...item, checked:!item.checked}:item)
+   setItems(listItems)
+   localStorage.setItem("todo_list" ,JSON.stringify(listItems))
+  }
+  const handleDelete=(id)=>{
+    const listItems =items.filter((item)=>item.id!==id)
+    setItems(listItems)
+    localStorage.setItem("todo_list" ,JSON.stringify(listItems))
+  }
+
   return (
     <main>
       <ul>
         
           {items.map((item)=>(
-            <li>
-              <input type="checkbox" cheked={item.checked}/>
-              <label>{item.item}</label>
-              <button>Delete</button>
+            <li className="item" key ={item.id}>
+              <input type="checkbox"
+              onChange={()=>handleCheck(item.id)} checked={item.checked}/>
+              <label style ={(item.checked)?{textDecoration:'line-through'}:null}
+               onDoubleClick={()=>handleCheck(item.id)}>{item.item}</label>
+              <FaTrashAlt 
+              role ="button"
+              onClick={()=>handleDelete(item.id)}
+              tabIndex="0" />
               </li>
           ))}
         
