fb1af95df002dafd7f9b0e7f386c1fad7fd4f605
Vijayakumar-Developer <vijai1993@gmail.com>
2024-05-26 16:55:03 +0530

    list and multiple components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
-
+import { useState } from 'react';
 function App() {
+
+  const [items,setItems]=useState(
+    [
+      {id:1,checked:true,item:"Learn React"},
+      {id:2,checked:false,item:"Understand React"},
+      {id:3,checked:false,item:"Practice React"}
+    ]
+  );
+
+  const handleCheck=(id) =>{
+   const listItems =items.map((item)=>
+    item.id===id ?{...item, checked:!item.checked}:item)
+   setItems(listItems)
+   localStorage.setItem("todo_list" ,JSON.stringify(listItems))
+  }
+  const handleDelete=(id)=>{
+    const listItems =items.filter((item)=>item.id!==id)
+    setItems(listItems)
+    localStorage.setItem("todo_list" ,JSON.stringify(listItems))
+  }
+
+ 
 
   return (
     <div className="App">
-          <Header />
-          <Content />
-          <Footer/>
+          <Header title="To Do List" />
+          <Content
+          items={items}
+          setItems={setItems}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete} />
+          <Footer
+          length={items.length}
+          />
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,49 +1,20 @@
 import React from 'react'
-import { useState } from 'react';
+import ItemsList from "./ItemsList";
 import { FaTrashAlt } from 'react-icons/fa';
 
 
-const Content = () => {
+const Content = ({items,setItems,handleCheck,handleDelete}) => {
    
-  const [items,setItems]=useState(
-    [
-      {id:1,checked:true,item:"Learn React"},
-      {id:2,checked:false,item:"Understand React"},
-      {id:3,checked:false,item:"Practice React"}
-    ]
-  );
-
-  const handleCheck=(id) =>{
-   const listItems =items.map((item)=>
-    item.id===id ?{...item, checked:!item.checked}:item)
-   setItems(listItems)
-   localStorage.setItem("todo_list" ,JSON.stringify(listItems))
-  }
-  const handleDelete=(id)=>{
-    const listItems =items.filter((item)=>item.id!==id)
-    setItems(listItems)
-    localStorage.setItem("todo_list" ,JSON.stringify(listItems))
-  }
-
+  
   return (
     <main>
       {(items.length)?(
-      <ul>
-        
-          {items.map((item)=>(
-            <li className="item" key ={item.id}>
-              <input type="checkbox"
-              onChange={()=>handleCheck(item.id)} checked={item.checked}/>
-              <label style ={(item.checked)?{textDecoration:'line-through'}:null}
-               onDoubleClick={()=>handleCheck(item.id)}>{item.item}</label>
-              <FaTrashAlt 
-              role ="button"
-              onClick={()=>handleDelete(item.id)}
-              tabIndex="0" />
-              </li>
-          ))}
-        
-      </ul>
+      <ItemsList 
+      items={items}
+          setItems={setItems}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+      />
       ) :(
         <p style={{marginTop:"2rem"}}>No Lists Found</p>
       )
