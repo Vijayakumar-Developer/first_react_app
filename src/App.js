1eda7f21758e08b704dd96069d49ddd04da9ae0b
Vijayakumar-Developer <vijai1993@gmail.com>
2024-06-05 16:24:52 +0530

    todo final app

--- src/App.js
+++ src/App.js
@@ -15,6 +15,15 @@ function App() {
 
   const [newItem,setnewItem]=useState('')
 
+  const addItem=(item) =>{
+
+    const id= items.length ?items[items.length -1].id+1:1;
+    const addNewItem= {id,checked:false,item}
+    const listItems = [...items,addNewItem]
+    setItems(listItems)
+    localStorage.setItem("todo_list" ,JSON.stringify(listItems))
+  }
+
   const handleCheck=(id) =>{
    const listItems =items.map((item)=>
     item.id===id ?{...item, checked:!item.checked}:item)
@@ -28,8 +37,11 @@ function App() {
   }
 
   const handleSubmit=(e)=>{
-    
-    console.log('e');
+    e.preventDefault()
+    if(!newItem) return ;
+    console.log(newItem);
+    addItem(newItem)
+    setnewItem('')
   }
 
  
